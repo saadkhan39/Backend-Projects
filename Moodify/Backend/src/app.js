4601ac55734d06d7661bb8f66efb67735c6de2e8
saadkhan39 <saadsajid928@gmail.com>
2026-05-02 12:58:27 +0530

    updated api url

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -6,11 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://backend-projects-kmxm.onrender.com"
-  ],
-  credentials: true
+    origin:"http://localhost:5173",
+    credentials:true
 }))
 app.use(express.static("./public"))
 
